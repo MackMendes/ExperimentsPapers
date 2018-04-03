81d739fef95018c73ced38e2a943fb6bc12128bf
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2018-04-03 01:21:27 -0300

    Implementação do Join entre os datasets de construção da linguagem, oracle e do reparos.

--- DescobrindoPadroesdeBugJS/CodeR/evaluation/StudyEvoluation.R
+++ DescobrindoPadroesdeBugJS/CodeR/evaluation/StudyEvoluation.R
@@ -1,4 +1,5 @@
 
+setwd("G:/Mestrado/Meus experimentos/Meu GitHub/ExperimentsPapers/DescobrindoPadroesdeBugJS/CodeR/evaluation")
 
 # ===========
 # Obter todos os conjuntos básicos de mudança
@@ -59,6 +60,7 @@ for(i in 1:dim(padroesDefeitos)[1]) {
 languageconstruct <- read.csv(file="original dataset/language_construct_database.csv", 
                                       header=TRUE, sep=";")
 
+names(languageconstruct)[1] <- "IDCommit"
 
 # ===========
 # Obter o dataset que o Hanan informou ter as informações de {Reparo, Label}
@@ -66,9 +68,14 @@ languageconstruct <- read.csv(file="original dataset/language_construct_database
 
 datasetLabelReparoCommit <- read.csv(file="original dataset/oracle.csv", 
                                      header=TRUE, sep=";")
-
+names(datasetLabelReparoCommit)
 
 # ===========
 # Realizando um Join entre os três datasets 
 # ===========
 
+library(dplyr)
+
+datasetOracleCompleted <- (languageconstruct %>% 
+                          inner_join(datasetLabelReparoCommit, by = c("IDCommit" = "IDCommit"))) %>%
+                          inner_join(padroesDefeitos[1:3], by = c("IDRepair" = "ID"))
